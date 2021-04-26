cedf2630c0a412224fe359ecf77755c304fcbe13
ConnorVEE <61746584+ConnorVEE@users.noreply.github.com>
2021-04-25 22:40:37 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Connor VanTress on 4/25/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
